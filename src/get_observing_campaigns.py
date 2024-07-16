d59fee16a09a9d8395f2871ff3484a382e9cad39
Aru Bhoop <37231424+aru-py@users.noreply.github.com>
2024-07-16 18:22:48 -0400

    now asynchronous; support for new campaign model

--- src/get_observing_campaigns.py
+++ src/get_observing_campaigns.py
@@ -1,50 +1,56 @@
 """
 get_observing_campaigns.py
 
-Fetches the content of an AAVSO Alert Notice and extracts specific information into a JSON object.
-
-Fields Extracted:
-    - Title
-    - Principal Investigator
-    - Abstract
-    - Justification
-    - Target Object
-    - Start Date
-    - End Date
-    - Status
+Fetches the content of AAVSO Alert Notices asynchronously and extracts specific information into a JSON object.
 
 Usage: pipenv run python get_alert_notices.py
 """
 
+import asyncio
 import json
 import os
 
+import aiohttp
 import fire
 import html2text
-import requests
 from bs4 import BeautifulSoup
-from openai import OpenAI
+from openai import AsyncOpenAI
+from tenacity import retry, wait_fixed, stop_after_attempt
 
 
-def fetch_alert_notice(notice_id):
-    """Retrieve the content of the given AAVSO Alert Notice."""
+async def fetch_alert_notice(session, notice_id):
+    """Retrieve the content of the given AAVSO Alert Notice asynchronously."""
+
+    # cache pages
+    try:
+        with open(f'data/pages/{notice_id}.txt', 'r') as f:
+            return f.read()
+    except FileNotFoundError:
+        pass
+
     alert_notice_url = f"https://www.aavso.org/aavso-alert-notice-{notice_id}"
-    response = requests.get(alert_notice_url)
-    soup = BeautifulSoup(response.content, 'html.parser').find('div', class_='main-container')
-    html_as_text = html2text.HTML2Text().handle(str(soup))
-    main_content = html_as_text[:-1200]
-    return main_content
+
+    async with session.get(alert_notice_url) as response:
+        content = await response.text()
+        soup = BeautifulSoup(content, 'html.parser').find('div', class_='main-container')
+        html_as_text = html2text.HTML2Text().handle(str(soup))
+        main_content = html_as_text[:-1200]
+        with open(f'data/pages/{notice_id}.txt', 'w') as f:
+            f.write(main_content)
+        return main_content
 
 
-def extract_information_from_page(openai_client, model, content, output_format):
-    """Use OpenAI to extract the desired fields from the AAVSO Alert Notice content."""
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(5))
+async def extract_information_from_page(openai_client, model, content, output_format):
+    """Use OpenAI to extract the desired fields from the AAVSO Alert Notice content asynchronously."""
+
     query = (
         "The following is an Observing Campaign published by the AAVSO. Extract the following information into a json object "
-        "Title, Principal Investigator, Abstract, Justification, Target Object, Start Date, End Date, and Status. "
-        "Unknown fields should be marked as N/A. Dates should be in the 'YYYY-MM-DD' format."
+        "Title, Principal Investigator, Abstract, Justification, Target Object, Spectral Lines, Filters, Start Date, End Date, and Status. "
+        "Unknown fields should be marked as N/A (or false for booleans). "
     )
 
-    chat_completion = openai_client.chat.completions.create(
+    chat_completion = await openai_client.chat.completions.create(
         model=model,
         messages=[{"role": "user", "content": f"{query}\n\n{content}"}],
         tools=[{"type": "function", "function": output_format}]
@@ -54,31 +60,56 @@ def extract_information_from_page(openai_client, model, content, output_format):
     return json.loads(output)
 
 
+async def process_alert_notice(session, openai_client, model, output_format, alert_notice_id):
+    print(f"Fetching content for alert notice {alert_notice_id}...")
+    content = await fetch_alert_notice(session, alert_notice_id)
+
+    if len(content) == 0:
+        return alert_notice_id, {}
+
+    print(f"Extracting information for alert notice {alert_notice_id}...")
+    try:
+        output = await extract_information_from_page(openai_client, model, content, output_format)
+    except Exception as e:
+        print(e)
+        return alert_notice_id, {"error": "Failed to extract information"}
+
+    return alert_notice_id, output
+
+
 async def process_alert_notices(start, end, out_file):
-    client = OpenAI()
+    client = AsyncOpenAI()
     model = os.environ.get("OPENAI_MODEL")
     with open("src/output_format.json") as f:
         output_format = json.load(f)
 
     alert_notices = range(start, end)
 
-    data = {}
+    with open(out_file, "r") as f:
+        json_data = json.load(f)
+        alert_notices = [notice_id for notice_id in alert_notices if 'error' in json_data[str(notice_id)]]
 
-    for alert_notice_id in alert_notices:
-        print(f"Fetching content for alert notice {alert_notice_id}...")
-        content = fetch_alert_notice(alert_notice_id)
+    print(alert_notices)
+    data = json_data
 
-        print("Extracting information...")
-        output = extract_information_from_page(client, model, content, output_format)
+    async with aiohttp.ClientSession() as session:
+        tasks = [process_alert_notice(session, client, model, output_format, alert_notice_id)
+                 for alert_notice_id in alert_notices]
+        results = await asyncio.gather(*tasks)
 
-        data[alert_notice_id] = output
+        for alert_notice_id, output in results:
+            data[alert_notice_id] = output
 
-        # Save data to file
-        with open(out_file, "w+") as f:
-            json.dump(data, f)
+            # Save data to file after each completed task
+            with open(out_file, "w+") as f:
+                json.dump(data, f)
 
     print(f"Data extraction completed and saved to {out_file}")
 
 
+def main(start, end, out_file):
+    asyncio.run(process_alert_notices(start, end, out_file))
+
+
 if __name__ == '__main__':
-    fire.Fire(process_alert_notices)
+    fire.Fire(main)
